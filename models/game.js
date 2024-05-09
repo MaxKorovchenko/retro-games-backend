9e79e3ddc3b7258c8bade5f424f612655d90f28e
MaxKorovchenko <max.korovchenko@gmail.com>
2024-05-09 11:26:21 +0300

    change addGameSchema and gameSchema

--- models/game.js
+++ models/game.js
@@ -12,6 +12,50 @@ const gameSchema = new Schema(
       enum: ["8-bit", "16-bit"],
       required: true,
     },
+    coverImageURL: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    gallery: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    genre: {
+      type: String,
+      enum: [
+        "action",
+        "adventure",
+        "RPG",
+        "strategy",
+        "simulation",
+        "puzzle",
+        "sports",
+        "platformer",
+        "shooter",
+        "fighting",
+        "racing",
+        "horror",
+        "educational",
+        "music",
+        "party",
+        "other",
+      ],
+      required: true,
+    },
+    numberOfPlayers: {
+      type: Number,
+      required: true,
+    },
+    releaseYear: {
+      type: Number,
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true }
 );
--- schemas/gamesSchemas.js
+++ schemas/gamesSchemas.js
@@ -3,6 +3,31 @@ const Joi = require("joi");
 const addGameSchema = Joi.object({
   title: Joi.string().required(),
   platform: Joi.string().valid("8-bit", "16-bit").required(),
+  coverImageURL: Joi.string().required(),
+  description: Joi.string().required(),
+  gallery: Joi.array().items(Joi.string()).required(),
+  genre: Joi.string()
+    .valid(
+      "action",
+      "adventure",
+      "RPG",
+      "strategy",
+      "simulation",
+      "puzzle",
+      "sports",
+      "platformer",
+      "shooter",
+      "fighting",
+      "racing",
+      "horror",
+      "educational",
+      "music",
+      "party",
+      "other"
+    )
+    .required(),
+  numberOfPlayers: Joi.number().required(),
+  releaseYear: Joi.number().required(),
 });
 
 module.exports = {
