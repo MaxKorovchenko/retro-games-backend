ebc1f7167702fab2972d7e39c687fab0e9223684
MaxKorovchenko <max.korovchenko@gmail.com>
2024-05-28 13:03:24 +0300

    change message in auth ctrl

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -105,7 +105,7 @@ const addToFavoriteGames = ctrlWrapper(async (req, res) => {
 
   return res.status(200).json({
     favoriteGames: user.favoriteGames,
-    message: "Game added to favorites successfully",
+    message: "The game was successfully added to your favorites",
   });
 });
 
@@ -135,7 +135,7 @@ const removeFromFavoriteGames = ctrlWrapper(async (req, res) => {
 
   return res.status(200).json({
     favoriteGames: user.favoriteGames,
-    message: "Game removed from favorites successfully",
+    message: "The game was successfully removed from your favorites",
   });
 });
 
